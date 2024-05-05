4b4b4489b7090867670d4a3581701dd709d876e2
Adhil Salim <bizadhil@gmail.com>
2024-05-05 21:39:11 +0530

    assets for user page

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -2,86 +2,95 @@ let db;
 let type;
 
 document.addEventListener("DOMContentLoaded", function () {
-    try {
-        let app = firebase.app();
-        let features = [
-            "auth",
-            "database",
-            "firestore",
-            "functions",
-            "messaging",
-            "storage",
-            "analytics",
-            "remoteConfig",
-            "performance",
-        ].filter((feature) => typeof app[feature] === "function");
+  try {
+    let app = firebase.app();
+    let features = [
+      "auth",
+      "database",
+      "firestore",
+      "functions",
+      "messaging",
+      "storage",
+      "analytics",
+      "remoteConfig",
+      "performance",
+    ].filter((feature) => typeof app[feature] === "function");
 
-        db = firebase.firestore();
+    db = firebase.firestore();
 
-        function domReady(fn) {
-            if (
-                document.readyState === "complete" ||
-                document.readyState === "interactive"
-            ) {
-                setTimeout(fn, 1000);
-            } else {
-                document.addEventListener("DOMContentLoaded", fn);
-            }
-        }
-
-        domReady(function () {
-            // If found you qr code
-            function onScanSuccess(decodeText, decodeResult) {
-                var result = JSON.parse(decodeText);
-                result.forEach((data) => {
-                    const registrationNumber = data.registrationNumber;
-                    if (registrationNumber) {
-                        getUserData(registrationNumber);
-                    }
-                });
-            }
-
-            let htmlscanner = new Html5QrcodeScanner("qr-reader", {
-                fps: 10,
-                qrbos: 250,
-            });
-            htmlscanner.render(onScanSuccess);
-        });
-    } catch (e) {
-        console.error(e);
+    function domReady(fn) {
+      if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+      ) {
+        setTimeout(fn, 1000);
+      } else {
+        document.addEventListener("DOMContentLoaded", fn);
+      }
     }
+
+    domReady(function () {
+      // If found you qr code
+      function onScanSuccess(decodeText, decodeResult) {
+        var result = JSON.parse(decodeText);
+        result.forEach((data) => {
+          const registrationNumber = data.registrationNumber;
+          if (registrationNumber) {
+            getUserData(registrationNumber);
+          }
+        });
+      }
+
+      let htmlscanner = new Html5QrcodeScanner("qr-reader", {
+        fps: 10,
+        qrbos: 250,
+      });
+      htmlscanner.render(onScanSuccess);
+    });
+  } catch (e) {
+    console.error(e);
+  }
 });
 
-
 function getUserData(id) {
-    try {
-        var registrationsRef = db.collection("registrations").doc(id);
+  try {
+    var registrationsRef = db.collection("registrations").doc(id);
 
-        registrationsRef
-            .get()
-            .then((doc) => {
-                if (doc.exists) {
-                    const data = doc.data();
-                    const heroSection = document.getElementById("hero-section");
+    registrationsRef
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          const data = doc.data();
+          const mainAttendanceBtn = document.getElementById("fbs-attendance-btn-main");
+          const heroSectionTitle = document.getElementById("fbs-title");
+          const heroSectionDescription =
+            document.getElementById("fbs-description");
 
-                    // Display a card that thanks the user for attending the TEDxAJCE 2024 event
-                    heroSection.innerHTML = `
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Dear ${data.name},</h5>
-                                <p class="card-text">Thank you for attending TEDxAJCE 2024 <br> We are glad to have you with us. Your registration number is ${data.id}</p>
-                            </div>
-                        </div>
-                    `;
-                } else {
-                    console.log("No such document!");
-                    alert("Could not find the user with the given registration number");
-                }
-            })
-            .catch((error) => {
-                console.log("Error getting document:", error);
-            });
-    } catch (e) {
-        console.error(e);
-    }
+          heroSectionTitle.innerHTML = `<span class="text-line"> Hello,</span>
+                    <span class="wow fadeInLeft" data-wow-delay=".4s">${
+                      data.name.split(" ")[0]
+                    }</span>`;
+          heroSectionDescription.style.display = "block";
+
+          heroSectionDescription.innerHTML = 
+          `<span class="text-line-small">
+                Thank you for attending TEDx AJCE 2024 🎉
+            </span><br />
+            <span class="text-line-small">
+                Your registration number is
+            <span>${data.id}</span></span>`;
+          
+            heroSectionDescription.style.display = "block";
+            mainAttendanceBtn.style.display = "none";
+        } else {
+          console.log("No such document!");
+          alert("Could not find the user with the given registration number");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+  } catch (e) {
+    console.error(e);
+  }
 }
